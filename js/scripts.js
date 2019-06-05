baa6a8e6dc6ee4f1fb56975f907d3cf418e7e810
Nathan Wangemann and Sam Cook <sam@>
2019-06-05 16:12:18 -0700

    create a better calculator with more function

--- js/scripts.js
+++ js/scripts.js
@@ -17,38 +17,54 @@
 	};
 	// when equals is clicked, it calls the correct function and outputs the answer
 
-$(document).ready(function() {
-	$(".add").click(function() {
-		var input1 = parseInt($(".input1").val());
-		var input2 = parseInt($(".input2").val());
-		var result = addition(input1, input2);
-		alert(result);
-		event.preventDefault();
-	});
-	$(".subtract").click(function() {
-		var input1 = parseInt($(".input1").val());
-		var input2 = parseInt($(".input2").val());
-		var result = subtraction(input1, input2);
-		alert(result);
-		event.preventDefault();
-	});
-	$(".multiply").click(function() {
-		var input1 = parseInt($(".input1").val());
-		var input2 = parseInt($(".input2").val());
-		var result = multiplication(input1, input2);
-		alert(result);
-		event.preventDefault();
-	});
-	$(".divide").click(function() {
-		var input1 = parseInt($(".input1").val());
-		var input2 = parseInt($(".input2").val());
-		var result = division(input1, input2);
-		alert(result);
-		event.preventDefault();
-	});
+ $(document).ready(function() {
+// 	$(".add").click(function() {
+// 		var input1 = parseInt($(".input1").val());
+// 		var input2 = parseInt($(".input2").val());
+// 		var result = addition(input1, input2);
+// 		alert(result);
+// 		event.preventDefault();
+// 	});
+// 	$(".subtract").click(function() {
+// 		var input1 = parseInt($(".input1").val());
+// 		var input2 = parseInt($(".input2").val());
+// 		var result = subtraction(input1, input2);
+// 		alert(result);
+// 		event.preventDefault();
+// 	});
+// 	$(".multiply").click(function() {
+// 		var input1 = parseInt($(".input1").val());
+// 		var input2 = parseInt($(".input2").val());
+// 		var result = multiplication(input1, input2);
+// 		alert(result);
+// 		event.preventDefault();
+// 	});
+// 	$(".divide").click(function() {
+// 		var input1 = parseInt($(".input1").val());
+// 		var input2 = parseInt($(".input2").val());
+// 		var result = division(input1, input2);
+// 		alert(result);
+// 		event.preventDefault();
+// 	});
+
+$(".equals").click(function() {
+	var symbols = [addition, subtraction, multiplication, division];
+ 	alert(symbols[parseInt($("#mathFunction").val())]((parseInt($(".input1").val())), (parseInt($(".input2").val()))));
+	event.preventDefault();
+});
+
 });
 
 
+// $(".equals").click(function() {
+// 	var symbols = [addition, subtraction, multiplication, division];
+// 	var input1 = parseInt($(".input1").val());
+// 	var input2 = parseInt($(".input2").val());
+// 	var selected = parseInt($("#mathFunction").val());
+// 	var result = (symbols[selected](input1, input2));
+// 	alert(result);
+// 	event.preventDefault();
+// });
 
 
 
@@ -57,8 +73,12 @@ $(document).ready(function() {
 
 
 
-
-
+//
+// $(".equals").click(function() {
+// 	var symbols = [addition, subtraction, multiplication, division];
+// 	alert(symbols[parseInt($("#mathFunction").val())](parseInt($(".input1").val()), parseInt($(".input2").val())));
+// 	event.preventDefault();
+// });
 
 
 
@@ -77,11 +97,4 @@ $(document).ready(function() {
 	//
 	// });
 	// 	alert(selected);
-	// });
-
-
-
-	// $ var equals = (".equals").click(function() {
-	// 	var symbols = [addition, subtraction, multiplication, division]
-	// 	alert(symbols[parseInt(".mathButton")] + (parseInt(".input1"), parseInt(".input2")));
 	// });
